783d1f43fe3e8d46dcf970c415f750e096eddb2d
andrew <tasks.androo@gmail.com>
2022-05-27 18:45:14 -0400

    Fetch transactions through jpeg.store api, instead of graphql. Estimate ADA value of each asset in collection.

--- rarity.py
+++ rarity.py
@@ -18,49 +18,19 @@ class Collection():
           }
         }
     """
-    tx_query = """
-        query transactionsByPolicy($policy_id: Hash28Hex, $offset: Int) {
-          transactions(
-            where: { outputs: { tokens: { asset: { policyId: { _eq: $policy_id } } } } }
-            limit: 2500
-            order_by: { block: { forgedAt: desc } }
-            offset: $offset
-          ) {
-            hash
-            inputs {
-              address
-              value
-              tokens {
-                asset {
-                  policyId
-                  assetName
-                }
-              }
-            }
-            outputs {
-              address
-              value
-              tokens {
-                asset {
-                  policyId
-                  assetName
-                }
-              }
-            }
-          }
-        }
-    """
 
     def __init__(self, name):
         self.name = name
         self.policy_id = str()
-        self.last_fetched_tx = str()
+        self.total_tx_count = 0
         self.assets = dict()
         self.facets = dict()
         self.properties = list()
+        self.rarity_unit_value = 0
 
         self.load_collection()
         self.fetch_assets()
+        self.fetch_transactions()
         self.set_properties()
         self.set_facets()
 
@@ -71,6 +41,7 @@ class Collection():
         output['facets'] = self.facets
         output['policy_id'] = self.policy_id
         output['properties'] = self.properties
+        output['tx_count'] = self.total_tx_count
         with open(f'collections/{self.name}.json', 'w') as file:
             json.dump(output, file, indent=2)
 
@@ -85,12 +56,13 @@ class Collection():
         self.facets = input['facets']
         self.policy_id = input['policy_id']
         self.properties = input['properties']
+        self.total_tx_count = input['tx_count']
 
     def fetch_assets(self):
-        if self.assets:
-            return
-
+        if self.policy_id: return
         self.policy_id = input('Enter policy ID: ')
+
+        if self.assets: return
 
         print(f'Fetching assets for collection [{self.name}]')
         offset = 0
@@ -117,11 +89,37 @@ class Collection():
 
             offset += 1
 
+    # Assets must be set
+    def fetch_transactions(self):
+        url = f'https://server.jpgstoreapis.com/collection/{self.policy_id}/transactions?'
+        params = { "page": 1, "count": 1 }
+
+        resp = requests.get(url, params=params).json()
+        num_to_fetch = resp["tot"]
+        num_to_fetch -= self.total_tx_count
+
+        params["count"] = num_to_fetch
+        print(f'Fetching {num_to_fetch} transactions for collection {self.name}.')
+        resp = requests.get(url, params=params).json()
+
+        for tx in resp["transactions"]:
+            for asset_name in self.assets:
+                metadata = self.assets[asset_name]
+                if tx["display_name"] == metadata["name"]:
+                    if metadata.get("sales") is None: metadata["sales"] = []
+
+                    amount = tx["amount_lovelace"]
+                    sales = metadata["sales"]
+                    if amount not in sales: sales.append(amount)
+                    print(json.dumps(sales, indent=2))
+
+        self.total_tx_count = resp["tot"]
+
     def set_properties(self):
         if self.properties:
             return
         attributes = dict()
-        print(f'Setting facets for collection [{self.name}]')
+        print(f'Setting properties for collection [{self.name}]')
         for asset in self.assets.values():
             for key, value in asset.items():
                 if key not in attributes:
@@ -140,7 +138,8 @@ class Collection():
             facet = f'{facet_type}.{property}'
             if facet not in self.facets:
                 self.facets[facet] = {}
-            elif value not in self.facets[facet]:
+            
+            if value not in self.facets[facet]:
                 self.facets[facet][value] = 1
             else:
                 self.facets[facet][value] += 1
@@ -149,7 +148,7 @@ class Collection():
         print('Setting facets.')
         for asset in self.assets.values():
             for property in self.properties:
-                value = asset[property]
+                value = asset.get(property) or "None"
                 if isinstance(value, dict):
                     for inner_property, inner_value in value.items():
                         increment_facet(inner_property, inner_value)
@@ -157,6 +156,7 @@ class Collection():
                     increment_facet(property, value)
 
 
+    # Properties and facets must be set
     def calc_statistical_rarity(self):
         def calc_rarity_score(prop, val):
             facet_type = 'numericAttributes' if val.isnumeric() else 'stringAttributes'
@@ -166,7 +166,7 @@ class Collection():
         for asset in self.assets.values():
             rarity_score = 0
             for property in self.properties:
-                value = asset[property]
+                value = asset.get(property) or "None"
                 if isinstance(value, dict):
                     for inner_property, inner_value in value.items():
                         rarity_score += calc_rarity_score(inner_property, inner_value)
@@ -174,69 +174,38 @@ class Collection():
                     rarity_score += calc_rarity_score(property, value)
             asset['rarity'] = rarity_score
 
+    # rarity and sales must be set
+    def calc_rarity_unit_value(self):
+        rarity_score_total =  0
+        lovelace_total = 0
+        for name in self.assets:
+            asset = self.assets[name]
+
+            sales = asset.get('sales')
+            if sales:
+                rarity_score_total += asset['rarity']
+                # lovelace_total += sum(sales) / len(sales)
+                lovelace_total += sales[0]
+        self.rarity_unit_value = ( lovelace_total / rarity_score_total ) / 1_000_000
+    
+    # rarity unit value must be set
+    def set_value_estimate(self):
+        for name in self.assets:
+            asset = self.assets[name]
+            asset['value'] = self.rarity_unit_value * asset['rarity']
+
     def sort_assets(self):
         self.assets = dict(
             sorted(self.assets.items(), key=lambda asset: -asset[1]['rarity']))
+            
 
-    def fetch_transactions(self):
-        offset = 0
-        while True:
-            variables = {"policy_id": f"{self.policy_id}",
-                         "offset": offset * 2500}
-            resp = requests.post(
-                self.endpoint, json={"query": self.tx_query, "variables": variables})
-            if resp.ok:
-                txs = resp.json()['data']['transactions']
-                if not txs:
-                    break
-                print('txs found')
-                for tx in txs:
-                    hash = tx['hash']
-                    inputs = tx['inputs']
-                    outputs = tx['outputs']
-                    # txs have never been fetched
-                    if not self.last_fetched_tx:
-                        self.last_fetched_tx = hash
-                    # pulled all recent txs
-                    elif self.last_fetched_tx == hash:
-                        self.last_fetched_tx = txs[0]['hash']
-                        break
-                    
-                    # spent_txs = dict()
-                    # for input in inputs:
-                    #     address = input['address']
-                    #     if address not in spent_txs:
-                    #         spent_txs[address] = dict()
-                    #         spent_txs[address]['value'] = input]
-
-                    print(len(inputs))
-                    print(len(outputs))
-
-                    # for input in inputs:
-                    #     tokens = input['tokens']
-                    #     for token in tokens:
-                    #         name = token['asset']['assetName']
-                    #         token['asset']['assetName'] = bytes.fromhex(name).decode('ascii')
-                    
-                    # for output in outputs:
-                    #     tokens = output['tokens']
-                    #     for token in tokens:
-                    #         name = token['asset']['assetName']
-                    #         token['asset']['assetName'] = bytes.fromhex(name).decode('ascii')
-                            
-                    print(json.dumps(inputs, indent=2))
-                    print()
-                    print(json.dumps(outputs, indent=2))
-                break
-        def parse_utxo(input, output):
-            # determine sale price of NFT
-            pass
 
 
 if __name__ == '__main__':
     name = input('Enter collection name: ')
     collection = Collection(name)
-    collection.fetch_transactions()
-    # collection.calc_statistical_rarity()
-    # collection.sort_assets()
-    # collection.save_collection()
+    collection.calc_statistical_rarity()
+    collection.calc_rarity_unit_value()
+    collection.set_value_estimate()
+    collection.sort_assets()
+    collection.save_collection()
